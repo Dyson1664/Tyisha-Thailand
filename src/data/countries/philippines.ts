006caf3548788313770a38190fb85d7126d42cf9
Dyson1664 <127164393+Dyson1664@users.noreply.github.com>
2026-07-09 14:30:51 +0700

    update

--- src/data/countries/philippines.ts
+++ src/data/countries/philippines.ts
@@ -8,6 +8,7 @@ import phCanyon2 from "@/assets/PH East/canyon_2.webp";
 import phNoah3 from "@/assets/PH East/noah-3.jpg";
 import phDay2Church from "@/assets/PH East/day2_church.webp";
 import phNoah2 from "@/assets/PH East/noah-2.webp";
+import phPb from "@/assets/PH East/pb.jpg";
 import phDesktopTurtle from "@/assets/PH East/desktop_turtle.webp";
 import phNoah4 from "@/assets/PH East/noah-4.webp";
 import phNoah5 from "@/assets/PH East/noah-5.jpg";
@@ -33,13 +34,13 @@ export const philippinesData = {
   heroImage: phDesktopTurtle,
   desktopHeroVideo: phMoalboalVideo,
   desktopHeroPosterImage: phTurtle,
-  desktopTopMiddleImage: phNoah2,
+  desktopTopMiddleImage: phBoracay1,
   desktopTopMiddleObjectPosition: "50% 75%",
   desktopTopRightImage: phDesktopTurtle,
   desktopTopRightObjectPosition: "50% 75%",
   desktopBottomLeftImage: phNoah5,
-  desktopBottomMiddleImage: phCanyon2,
-  desktopBottomMiddleObjectPosition: "50% 85%",
+  desktopBottomMiddleImage: phPb,
+  desktopBottomMiddleObjectPosition: "center top",
   desktopBottomRightImage: phNoah1,
   desktopBottomRightObjectPosition: "50% 78%",
   hideSummary: true,
@@ -175,7 +176,7 @@ export const philippinesData = {
       day: 5,
       title: "Moalboal to Boracay",
       location: "Boracay",
-      heroImage: phBoracay1,
+      heroImage: phNoah2,
       description:
         "Paradise is calling. Today, we travel back to Cebu Airport and catch a short flight to Caticlan, the gateway to Boracay. From there, we hop over to the island and check into our resort, our home for the next three nights. Boracay is famous for powdery white sand, bright blue water, beach clubs, sunsets, and a nightlife scene that does not mess around. Once we are settled in, we will head out to watch the sunset. After that, it is time for a group dinner and a night out for anyone ready to experience Boracay after dark. Want to chill instead? Completely fair. Massage, shower, early night, and no one needs to know.",
       meals: "1 breakfast",
